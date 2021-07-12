bcca0280576df188ea0282250faed839216f3233
Juan Quattordio <juanquattordio@gmail.com>
2021-07-12 18:00:12 -0300

    Se crean componentes iniciales

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CalcAvanzada from './CalcAvanzada';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalcAvanzada />
   </React.StrictMode>,
   document.getElementById('root')
 );
