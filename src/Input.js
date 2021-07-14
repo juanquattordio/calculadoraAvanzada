d7f542fcfed28882ea0d6d29a48a3ab0bb032b8a
Juan Quattordio <juanquattordio@gmail.com>
2021-07-14 00:30:17 -0300

    Prueba lectura ingreso números

--- src/Input.js
+++ src/Input.js
@@ -6,11 +6,13 @@ class Input extends Component {
     changeValue = (e) => {
         this.props.onChange(e.target.value, [e.target.name])
     }
-
+    console = (e) => {
+        console.log("Key" + e.key + " code " + e.code)
+    }
     render() {
         return (
             <div>
-                <input type="text" name={this.props.name} onChange={this.changeValue} value={this.props.valor}>
+                <input type="text" name={this.props.name} onChange={this.changeValue} value={this.props.valor} onKeyDown={this.console}>
                 </input>
 
             </div>
