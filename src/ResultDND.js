53e4d23ab8e5cda0ca61b3525b65f97f44674446
Juan Quattordio <juanquattordio@gmail.com>
2021-07-16 23:08:16 -0300

    Se crea metodo de calculo y mensajes. Se podría mejorar mensaje en cálculo de raíz de inputs por separado

--- src/ResultDND.js
+++ src/ResultDND.js
@@ -6,32 +6,37 @@ function ResultDND(props) {
     let [message, setMessage] = useState("");
 
     useEffect(() => {
-        calcular(props.operation);
+        calcular(props.operation.simbol);
+        mensaje(props.operation.operation, props.operation.simbol);
     }, [props])
 
     let calcular = (operacion) => {
         let a = parseFloat(props.value);
         let b = parseFloat(props.value2);
-        let message1;
-        let message2;
-        if (b !== 0) {
-            message1 = "Error"
-        } else {
-            message1 = "El resultado de la " + operacion.operation + " de " + a + " y " + b + " es:";
-            message2 = "El resultado de la " + operacion.operation + " de " + a + " es:";
-        };
-        switch (operacion.simbol) {
-            case "+": setResultado(a + b); setMessage(message1); break;
-            case "-": setResultado(a - b); setMessage(message1); break;
-            case "*": setResultado(a * b); setMessage(message1); break;
-            case "/": setResultado(a / b); setMessage(message1); break;
-            case "^": setResultado(a / b); setMessage(message1); break;
-            case "^-2": setResultado(a / b); setMessage(message1); break;
-            default: setResultado(a); setMessage(message2); break;
+        switch (operacion) {
+            case "+": setResultado(a + b); break;
+            case "-": setResultado(a - b); break;
+            case "*": setResultado(a * b); break;
+            case "/": setResultado((a / b).toFixed(3)); break;
+            case "^": setResultado((Math.pow(a, b))); break;
+            case "^-2": setResultado((Math.sqrt(a)).toFixed(3)); break;
+            case "hyp": setResultado((Math.hypot(a, b)).toFixed(3)); break;
+            default: break;
         }
     }
-    let a = parseFloat(props.value);
-    let b = parseFloat(props.value2);
+    let mensaje = (opearcion, simbolo) => {
+        let a = parseFloat(props.value);
+        let b = parseFloat(props.value2);
+        let message;
+        if (b === 0 && simbolo === "/" || (b < 0 || a < 0) && simbolo === "^-2") {
+            message = "Error al calcular la " + opearcion;
+            setResultado("");
+        }
+        else {
+            message = "El resultado de la " + opearcion + " de " + a + " y " + b + " es: ";
+        };
+        setMessage(message);
+    }
 
     const [{ isDragging }, drag] = useDrag(() => ({
         type: "div",
@@ -42,7 +47,7 @@ function ResultDND(props) {
     return (
         <div className="border">
             <p id="Result" ref={drag} style={{ border: isDragging ? "5px solid red" : "0px" }}>
-                {message}  {resultado} </p>
+                {message}{resultado} </p>
         </div>
     )
 }
